7ba8d3642e5ddc66c6264a659f92e942fea9db2f
Brandan Haertel <foilpudding@gmail.com>
2014-07-30 13:43:21 -0700

    uploaded code

---
+++ mechgame/Assets/Rescources/Scripts/CollisionKillCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionKillCamera : MonoBehaviour {
+
+	GameObject camera;
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectsWithTag ("MainCamera")[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print (camera.transform.position);
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+
+		if(col.gameObject.tag == "Player")
+		{
+			print("kill");
+			col.gameObject.transform.position = new Vector3(4,5,6);
+		}
+	}
+}
---
+++ mechgame/Assets/Rescources/Scripts/sphere.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphere : MonoBehaviour {
+
+	private float kHeroSpeed = .05f;
+	public float kHeroRotateSpeed = 90f/1f; //90deg in 1 seconds
+	private float ypos = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += kHeroSpeed * (Input.GetAxis ("Vertical")) * (-1 * this.transform.forward);
+		transform.Rotate((-1 * Vector3.up), -1f * Input.GetAxis("Horizontal") * (kHeroRotateSpeed * Time.smoothDeltaTime));
+	}
+}
